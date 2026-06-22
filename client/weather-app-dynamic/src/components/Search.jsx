5685506859d45cf8d90feb4724d5ff01059159bc
mmuneeb1000 <muneebhashmi8@gmail.com>
2026-06-22 19:37:45 +0500

    syntax correction

--- client/weather-app-dynamic/src/components/Search.jsx
+++ client/weather-app-dynamic/src/components/Search.jsx
@@ -2,13 +2,13 @@ function Search() {
   return (
     <section className="search">
       <h1>How's the sky looking today?</h1>
-      <div className="search-box">
+      <form className="search-box">
         <input
-          itemType="search"
+          type="tert"
           placeholder="Search for a city, e.g., New York Search Feels like"
         ></input>
         <button>Search</button>
-      </div>
+      </form>
     </section>
   );
 }
